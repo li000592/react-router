a2b45b60b5bfd671636ff8635fdd1f468ba2bad1
li000592 <li000592@algonquinlive.com>
2020-10-16 02:11:15 -0400

    fix NavLink Bug

--- src/PostsList.js
+++ src/PostsList.js
@@ -4,16 +4,17 @@ import Post from './Post'
 
 
 function PostsList(props) {
+    console.log(props)
     let { id } = useParams()
     console.log("ID:===", id);
     const cards = props.posts.map(post => {
-        if (post.userId === id.toString()) return <Post key={post.id} post={post} />
+        if (post.userId == id.toString()) return <Post key={post.id} post={post} />
     })
     return (
         <div>
-            <button >
-                <NavLink className="NavLink" to='/'>Home</NavLink>
-            </button>
+
+            <NavLink className="NavLink" to='/'>Home</NavLink>
+
             <div className='card-container'>{cards}</div>
         </div>
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as NavLink, useParams } from "react-router-dom";
+import { NavLink, useParams } from 'react-router-dom'
 import Todo from './Todo'
 
 function TodoList(props) {
     let { id } = useParams()
     console.log("ID:===", id);
     const cards = props.todos.map(todo => {
-        if (todo.userId === id) return <Todo key={todo.id} todo={todo} />
+        if (todo.userId == id) return <Todo key={todo.id} todo={todo} />
     })
     return (
         <div>
-            <button >
-                <NavLink className="NavLink" to='/'>Home1</NavLink>
-            </button>
+
+            <NavLink className="NavLink" to='/'>Home</NavLink>
+
             <div className='card-container'>{cards}</div>
         </div>
     )
